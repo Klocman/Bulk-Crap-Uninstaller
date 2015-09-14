a9f6933da637b8f6e42b85767a18887f7d439937
Marcin Szeniak <bugs.klocman@gmail.com>
2015-09-14 22:20:58 +0200

    Bump up version number to 2.12

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -27,4 +27,4 @@
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.11.0.0")]
+[assembly: AssemblyVersion("2.12.0.0")]
