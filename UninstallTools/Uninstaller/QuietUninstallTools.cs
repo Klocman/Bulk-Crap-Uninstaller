ad26227291cac5fc8c6158e94ec30c06683cade9
Marcin Szeniak <marc200333@gmail.com>
2016-02-26 21:08:28 +0100

    Cleanup of QuietUninstallTools.cs

--- UninstallTools/Uninstaller/QuietUninstallTools.cs
+++ UninstallTools/Uninstaller/QuietUninstallTools.cs
@@ -10,6 +10,7 @@ namespace UninstallTools.Uninstaller
     public static class QuietUninstallTools
     {
         private static string _assemblyLocation;
+
         private static string AssemblyLocation
         {
             get
@@ -17,21 +18,26 @@ namespace UninstallTools.Uninstaller
                 if (_assemblyLocation == null)
                 {
                     _assemblyLocation = Assembly.GetExecutingAssembly().Location;
-                    if (_assemblyLocation.ContainsAny(new[] { ".dll", ".exe" }, StringComparison.OrdinalIgnoreCase))
+                    if (_assemblyLocation.ContainsAny(new[] {".dll", ".exe"}, StringComparison.OrdinalIgnoreCase))
                         _assemblyLocation = PathTools.GetDirectory(_assemblyLocation);
                 }
                 return _assemblyLocation;
             }
         }
 
-        private static string UninstallerAutomatizerPath => Path.Combine(AssemblyLocation, @"UninstallerAutomatizer.exe");
+        private static string UninstallerAutomatizerPath
+            => Path.Combine(AssemblyLocation, @"UninstallerAutomatizer.exe");
 
         /// <summary>
-        /// Generate missing quiet commands if possible
+        ///     Generate missing quiet commands if possible
         /// </summary>
         /// <param name="entries">Entries to generate the quiet commands for</param>
-        /// <param name="automatizerKillstuck">Generated entries should kill the process if it gets stuck waiting for user input or the process otherwise fails</param>
-        public static void GenerateQuietCommands(IEnumerable<ApplicationUninstallerEntry> entries, bool automatizerKillstuck)
+        /// <param name="automatizerKillstuck">
+        ///     Generated entries should kill the process if it gets stuck waiting for user input or
+        ///     the process otherwise fails
+        /// </param>
+        public static void GenerateQuietCommands(IEnumerable<ApplicationUninstallerEntry> entries,
+            bool automatizerKillstuck)
         {
             var nsisCommandStart = $"\"{UninstallerAutomatizerPath}\" {UninstallerType.Nsis} ";
             if (automatizerKillstuck)
@@ -48,18 +54,20 @@ namespace UninstallTools.Uninstaller
                         uninstallerEntry.QuietUninstallString = nsisCommandStart + uninstallerEntry.UninstallString;
                         break;
 
-                        case UninstallerType.InnoSetup:
+                    case UninstallerType.InnoSetup:
                         try
                         {
+                            // Get rid of quotes and arguments that are already there for some weird reason (InnoSetup doesn't need any arguments)
                             uninstallerEntry.QuietUninstallString =
                                 $"\"{ProcessTools.SeparateArgsFromCommand(uninstallerEntry.UninstallString).FileName}\" /SILENT";
                         }
                         catch (ArgumentException)
-                        { }
+                        {
+                        }
                         catch (FormatException)
-                        { }
+                        {
+                        }
                         break;
-
                 }
             }
         }
