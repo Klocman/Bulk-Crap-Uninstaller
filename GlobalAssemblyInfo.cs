3ac47a777fe791027f6923e30158c22de42ce673
Marcin Szeniak <marc200333@gmail.com>
2016-03-13 11:23:55 +0100

    Version up to 3.3.1

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -27,4 +27,4 @@
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.3.0.0")]
+[assembly: AssemblyVersion("3.3.1.0")]
