b2db7cdc5c75c53a221ab417e0230a2ee00751f3
Marcin Szeniak <marc200333@gmail.com>
2016-03-21 13:27:55 +0100

    Added Hungarian translation by Phoenix (Döbröntei Sándor) Fixed some translations not being referenced in the project properly

---
+++ BulkCrapUninstaller/Controls/UninstallerList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BulkCrapUninstaller.Controls
+{
+    public partial class UninstallerList : UserControl
+    {
+        public UninstallerList()
+        {
+            InitializeComponent();
+            /*
+            uninstallerObjectListView.DataBindings.Add(nameof(uninstallerObjectListView.SelectedItems), treeListView1,
+                nameof(uninstallerObjectListView.SelectedItems));
+            uninstallerObjectListView.DataBindings.Add(nameof(uninstallerObjectListView.), treeListView1,
+                nameof(uninstallerObjectListView.SelectedItems));*/
+        }
+    }
+}
