d25da2c3af646fab41e254d4e38964258432e09e
Marcin Szeniak <marc200333@gmail.com>
2016-07-27 12:39:12 +0200

    Add project for SteamHelper

---
+++ SteamHelper/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamHelper
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SteamHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("SteamHelper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("SteamHelper")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("43022440-594d-437c-96c8-6aa24f6cee63")]
