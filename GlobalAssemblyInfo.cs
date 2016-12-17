bdf5a325d66754d90cb2f87e5f8879a36a9b064c
Marcin Szeniak <marc200333@gmail.com>
2016-12-17 02:11:09 +0100

    Version bump to 3.5.0.*

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -27,4 +27,4 @@
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("3.4.2.*")]
+[assembly: AssemblyVersion("3.5.0.*")]
