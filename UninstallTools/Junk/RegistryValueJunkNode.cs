554e5a4bb04c46b6043e08f22d5891bedfa3b1a7
Marcin Szeniak <marc200333@gmail.com>
2017-01-23 23:02:41 +0100

    Fixed RegistryValueJunkNode backup failing if value name has a dot in its name. Fixes #47

--- UninstallTools/Junk/RegistryValueJunkNode.cs
+++ UninstallTools/Junk/RegistryValueJunkNode.cs
@@ -21,7 +21,7 @@ namespace UninstallTools.Junk
             using (var key = RegistryTools.OpenRegistryKey(ParentPath))
             {
                 var dir = CreateBackupDirectory(backupDirectory);
-                var fileName = PathTools.SanitizeFileName(FullName.TrimStart('\\')) + ".reg";
+                var fileName = PathTools.SanitizeFileName(FullName.TrimStart('\\').Replace('.', '_')) + ".reg";
                 RegistryTools.ExportRegistryStringValues(Path.Combine(dir, fileName), ParentPath,
                     new KeyValuePair<string, string>(Name, key.GetValue(Name)?.ToString()));
             }
