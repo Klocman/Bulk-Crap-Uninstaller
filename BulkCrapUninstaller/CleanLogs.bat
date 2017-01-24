81d47b0db99a37cd6c582a6e61613279f395b4f7
Marcin Szeniak <marc200333@gmail.com>
2017-01-24 21:51:42 +0100

    Added removal of .NET generated setting drectories in AppData to the portable clanup

--- BulkCrapUninstaller/CleanLogs.bat
+++ BulkCrapUninstaller/CleanLogs.bat
@@ -1,12 +1,12 @@
 title Removing BCU log files...
-@echo off
-cls
+rem @echo off
+rem cls
 
 if not exist %LOCALAPPDATA% (
 REM LOCALAPPDATA doesn't exist on windows older than Vista
-set MSLOGDIR=%APPDATA%\Microsoft
+set APPDATASAFE=%APPDATA%
 ) else (
-set MSLOGDIR=%LOCALAPPDATA%\Microsoft
+set APPDATASAFE=%LOCALAPPDATA%
 )
 
 REM Wait for BCU to exit so the logs are written
@@ -24,9 +24,26 @@ ping -n 2 127.0.0.1 >nul
 
 REM Move to the log dir and remove all logs. They can be under following directories:
 REM \CLR_v2.0\UsageLogs \CLR_v2.0_32\UsageLogs \CLR_v4.0\UsageLogs \CLR_v4.0_32\UsageLogs
-cd /d "%MSLOGDIR%"
+cd /d "%APPDATASAFE%\Microsoft"
 
 echo Deleting Assembly Usage Logs...
 del /f /s BCUninstaller.exe.log SteamHelper.exe.log StoreAppHelper.exe.log UninstallerAutomatizer.exe.log UpdateHelper.exe.log
+
+echo Deleting .NET generated drectories...
+REM Settings directory automatically created by .NET
+set SETTINGSDIR=%APPDATASAFE%\Marcin_Szeniak
+
+if not exist %SETTINGSDIR% (
+exit
+)
+
+REM Remove all directories related to BCU
+for /d %%G in ("%SETTINGSDIR%\BCUninstaller*") do rd /s /q "%%~G"
+
+REM Check if the settings dir is empty. If there are directories left, exit.
+for /d %%i in ("%SETTINGSDIR%\*") do exit
+
+REM Dir is empty, so remove it.
+rd "%SETTINGSDIR%"
 
 exit
