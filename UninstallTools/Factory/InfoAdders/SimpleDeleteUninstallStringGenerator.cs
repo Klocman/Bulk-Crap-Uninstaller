0a7b45300d4c4bcdcfd5ba56ec4eb110a14a49d1
Marcin Szeniak <marc200333@gmail.com>
2017-02-03 02:14:50 +0100

    Added licence header

--- UninstallTools/Factory/InfoAdders/SimpleDeleteUninstallStringGenerator.cs
+++ UninstallTools/Factory/InfoAdders/SimpleDeleteUninstallStringGenerator.cs
@@ -1,3 +1,8 @@
+/*
+    Copyright (c) 2017 Marcin Szeniak (https://github.com/Klocman/)
+    Apache License Version 2.0
+*/
+
 namespace UninstallTools.Factory.InfoAdders
 {
     public class SimpleDeleteUninstallStringGenerator : IMissingInfoAdder
