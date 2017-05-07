7d672dceed606783395dd9db3c2df92448ac1f87
Marcin Szeniak <marc200333@gmail.com>
2017-05-07 22:48:55 +0200

    Added more granual language selection (by country)

--- source/BulkCrapUninstaller/CultureConfigurator.cs
+++ source/BulkCrapUninstaller/CultureConfigurator.cs
@@ -1,16 +1,11 @@
-﻿/*
-    Copyright (c) 2017 Marcin Szeniak (https://github.com/Klocman/)
-    Apache License Version 2.0
-*/
-
+﻿using BulkCrapUninstaller.Properties;
+using Klocman.Extensions;
+using Klocman.Tools;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading;
-using BulkCrapUninstaller.Properties;
-using Klocman.Extensions;
-using Klocman.Tools;
 
 namespace BulkCrapUninstaller
 {
@@ -18,28 +13,87 @@ namespace BulkCrapUninstaller
     {
         private static IEnumerable<CultureInfo> _supportedLanguages;
         private static CultureInfo _enUsCulture;
-        private static CultureInfo EnUsCulture => _enUsCulture ?? (_enUsCulture = CultureInfo.GetCultureInfo("en-US"));
 
-        public static IEnumerable<CultureInfo> SupportedLanguages
+        private static CultureInfo EnUsCulture => 
+            _enUsCulture ?? (_enUsCulture = CultureInfo.GetCultureInfo("en-US"));
+
+        public static IEnumerable<CultureInfo> SupportedLanguages => 
+            _supportedLanguages ?? (_supportedLanguages = GetSupportedLanguages());
+
+        private static IEnumerable<CultureInfo> GetSupportedLanguages()
         {
-            get
+            return new[]
             {
-                return _supportedLanguages ?? (_supportedLanguages = new[]
-                {
-                    EnUsCulture,
-                    CultureInfo.GetCultureInfo("en-GB"),
-                    CultureInfo.GetCultureInfo("cs-CZ"),
-                    CultureInfo.GetCultureInfo("de-DE"),
-                    CultureInfo.GetCultureInfo("es-ES"),
-                    CultureInfo.GetCultureInfo("fr-FR"),
-                    CultureInfo.GetCultureInfo("hu-HU"),
-                    CultureInfo.GetCultureInfo("it-IT"),
-                    CultureInfo.GetCultureInfo("pl-PL"),
-                    CultureInfo.GetCultureInfo("pt-PT"),
-                    CultureInfo.GetCultureInfo("ru-RU"),
-                    CultureInfo.GetCultureInfo("sl-SI")
-                }.OrderBy(x => x.DisplayName).ToList().AsEnumerable());
-            }
+                // en - English
+                EnUsCulture, //CultureInfo.GetCultureInfo("en-US"),
+                CultureInfo.GetCultureInfo("en-AU"),
+                CultureInfo.GetCultureInfo("en-BZ"),
+                CultureInfo.GetCultureInfo("en-CA"),
+                CultureInfo.GetCultureInfo("en-CB"),
+                CultureInfo.GetCultureInfo("en-IE"),
+                CultureInfo.GetCultureInfo("en-JM"),
+                CultureInfo.GetCultureInfo("en-NZ"),
+                CultureInfo.GetCultureInfo("en-PH"),
+                CultureInfo.GetCultureInfo("en-ZA"),
+                CultureInfo.GetCultureInfo("en-TT"),
+                CultureInfo.GetCultureInfo("en-GB"),
+                CultureInfo.GetCultureInfo("en-ZW"),
+
+                // de - German
+                CultureInfo.GetCultureInfo("de-AT"),
+                CultureInfo.GetCultureInfo("de-DE"),
+                CultureInfo.GetCultureInfo("de-LI"),
+                CultureInfo.GetCultureInfo("de-LU"),
+                CultureInfo.GetCultureInfo("de-CH"),
+
+                // es - Spanish
+                CultureInfo.GetCultureInfo("es-AR"),
+                CultureInfo.GetCultureInfo("es-BO"),
+                CultureInfo.GetCultureInfo("es-CL"),
+                CultureInfo.GetCultureInfo("es-CO"),
+                CultureInfo.GetCultureInfo("es-CR"),
+                CultureInfo.GetCultureInfo("es-DO"),
+                CultureInfo.GetCultureInfo("es-EC"),
+                CultureInfo.GetCultureInfo("es-SV"),
+                CultureInfo.GetCultureInfo("es-GT"),
+                CultureInfo.GetCultureInfo("es-HN"),
+                CultureInfo.GetCultureInfo("es-MX"),
+                CultureInfo.GetCultureInfo("es-NI"),
+                CultureInfo.GetCultureInfo("es-PA"),
+                CultureInfo.GetCultureInfo("es-PY"),
+                CultureInfo.GetCultureInfo("es-PE"),
+                CultureInfo.GetCultureInfo("es-PR"),
+                CultureInfo.GetCultureInfo("es-ES"),
+                CultureInfo.GetCultureInfo("es-UY"),
+                CultureInfo.GetCultureInfo("es-VE"),
+
+                // fr - French
+                CultureInfo.GetCultureInfo("fr-BE"),
+                CultureInfo.GetCultureInfo("fr-CA"),
+                CultureInfo.GetCultureInfo("fr-FR"),
+                CultureInfo.GetCultureInfo("fr-LU"),
+                CultureInfo.GetCultureInfo("fr-MC"),
+                CultureInfo.GetCultureInfo("fr-CH"),
+
+                // it - Italian
+                CultureInfo.GetCultureInfo("it-IT"),
+                CultureInfo.GetCultureInfo("it-CH"),
+
+                // pt - Portuguese
+                CultureInfo.GetCultureInfo("pt-PT"),
+                CultureInfo.GetCultureInfo("pt-BR"),
+
+                // nl - Dutch
+                //CultureInfo.GetCultureInfo("nl-NL"),
+                //CultureInfo.GetCultureInfo("nl-BE"),
+
+                // Misc languages
+                CultureInfo.GetCultureInfo("cs-CZ"),
+                CultureInfo.GetCultureInfo("hu-HU"),
+                CultureInfo.GetCultureInfo("pl-PL"),
+                CultureInfo.GetCultureInfo("ru-RU"),
+                CultureInfo.GetCultureInfo("sl-SI")
+            }.OrderBy(x => x.DisplayName).ToList().AsEnumerable();
         }
 
         public static void SetupCulture()
@@ -51,7 +105,7 @@ namespace BulkCrapUninstaller
             {
                 try
                 {
-                    currentCulture = SupportedLanguages.First(x=>x.Name.Equals(targetLocale));
+                    currentCulture = SupportedLanguages.First(x => x.Name.Equals(targetLocale));
                 }
                 catch
                 {
