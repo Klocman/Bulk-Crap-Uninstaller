b01ee79b3651233e363b699e168adcac90e92513
Marcin Szeniak <marc200333@gmail.com>
2017-05-11 22:03:53 +0200

    Small speedup of list item sorting

--- source/ObjectListView/ObjectListView.cs
+++ source/ObjectListView/ObjectListView.cs
@@ -4542,6 +4542,25 @@ namespace BrightIdeasSoftware
             return -1;
         }
 
+        private int GetDisplayOrderOfItemIndex(ListViewItem listViewItem)
+        {
+            if (!this.ShowGroups || this.Groups.Count == 0)
+                return listViewItem.Index;
+            
+            int i = 0;
+            foreach (ListViewGroup lvg in this.Groups)
+            {
+                foreach (ListViewItem lvi in lvg.Items)
+                {
+                    if (lvi == listViewItem)
+                        return i;
+                    i++;
+                }
+            }
+
+            return -1;
+        }
+
         /// <summary>
         /// Return the ListViewItem that appears immediately before the given item.
         /// If the given item is null, the last item in the list will be returned.
@@ -7943,7 +7962,7 @@ namespace BrightIdeasSoftware
             olvi.UseItemStyleForSubItems = true;
             olvi.SubItems.Clear();
             this.FillInValues(olvi, olvi.RowObject);
-            this.PostProcessOneRow(olvi.Index, this.GetDisplayOrderOfItemIndex(olvi.Index), olvi);
+            this.PostProcessOneRow(olvi.Index, this.GetDisplayOrderOfItemIndex(olvi), olvi);
         }
 
         /// <summary>
@@ -8608,7 +8627,9 @@ namespace BrightIdeasSoftware
             if (modelObject == null)
                 return null;
 
-            foreach (OLVListItem olvi in this.Items) {
+            for (int i = 0; i < this.Items.Count; i++)
+            {
+                var olvi = this.Items[i] as OLVListItem;
                 if (olvi.RowObject != null && olvi.RowObject.Equals(modelObject))
                     return olvi;
             }
