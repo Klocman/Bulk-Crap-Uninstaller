52043cd61c49bd0c0fc5489a162df232af675b94
Marcin Szeniak <marc200333@gmail.com>
2017-07-18 19:04:00 +0200

    Fixed possible crash on slow PCs when starting quiet uninstalls.

--- source/UninstallTools/Uninstaller/BulkUninstallEntry.cs
+++ source/UninstallTools/Uninstaller/BulkUninstallEntry.cs
@@ -268,172 +268,180 @@ namespace UninstallTools.Uninstaller
                 using (var uninstaller = UninstallerEntry.RunUninstaller(options.PreferQuiet, options.Simulate))
                 {
                     // Can be null during simulation
-                    if (uninstaller != null)
+                    if (uninstaller == null) return;
+
+                    if (options.PreferQuiet && UninstallerEntry.QuietUninstallPossible)
                     {
-                        if (options.PreferQuiet && UninstallerEntry.QuietUninstallPossible)
-                            uninstaller.PriorityClass = ProcessPriorityClass.BelowNormal;
-
-                        var checkCounters = options.PreferQuiet && options.AutoKillStuckQuiet &&
-                                            UninstallerEntry.QuietUninstallPossible;
-                        var watchedProcesses = new List<Process> {uninstaller};
-                        var idleCounter = 0;
-
-                        while (true)
+                        try
                         {
-                            if (_skipLevel == SkipCurrentLevel.Skip)
-                                break;
+                            uninstaller.PriorityClass = ProcessPriorityClass.BelowNormal;
+                        }
+                        catch
+                        {
+                            // Don't care if setting this fails
+                        }
+                    }
 
-                            foreach (var watchedProcess in watchedProcesses.ToList())
+                    var checkCounters = options.PreferQuiet && options.AutoKillStuckQuiet &&
+                                        UninstallerEntry.QuietUninstallPossible;
+                    var watchedProcesses = new List<Process> {uninstaller};
+                    var idleCounter = 0;
+
+                    while (true)
+                    {
+                        if (_skipLevel == SkipCurrentLevel.Skip)
+                            break;
+
+                        foreach (var watchedProcess in watchedProcesses.ToList())
+                            watchedProcesses.AddRange(watchedProcess.GetChildProcesses());
+
+                        // Msiexec service can start processes, but we don't want to watch the service
+                        if (UninstallerEntry.UninstallerKind == UninstallerType.Msiexec)
+                        {
+                            foreach (var watchedProcess in Process.GetProcessesByName("msiexec"))
                                 watchedProcesses.AddRange(watchedProcess.GetChildProcesses());
+                        }
 
-                            // Msiexec service can start processes, but we don't want to watch the service
-                            if (UninstallerEntry.UninstallerKind == UninstallerType.Msiexec)
+                        // Remove duplicate, dead, and blacklisted processes
+                        watchedProcesses = watchedProcesses.DistinctBy(x => x.Id).Where(p =>
+                        {
+                            try
                             {
-                                foreach (var watchedProcess in Process.GetProcessesByName("msiexec"))
-                                    watchedProcesses.AddRange(watchedProcess.GetChildProcesses());
+                                if (p.HasExited)
+                                    return false;
+
+                                var pName = p.ProcessName;
+                                if (NamesOfIgnoredProcesses.Any(n =>
+                                    pName.Equals(n, StringComparison.InvariantCultureIgnoreCase)))
+                                    return false;
+                            }
+                            catch (Win32Exception)
+                            {
+                                return false;
+                            }
+                            catch (InvalidOperationException)
+                            {
+                                return false;
                             }
 
-                            // Remove duplicate, dead, and blacklisted processes
-                            watchedProcesses = watchedProcesses.DistinctBy(x => x.Id).Where(p =>
+                            return !processSnapshot.Contains(p.Id);
+                        }).ToList();
+
+                        // Check if we are done, or if there are some proceses left that we missed
+                        if (watchedProcesses.Count == 0)
+                        {
+                            if (string.IsNullOrEmpty(UninstallerEntry.InstallLocation))
+                                break;
+
+                            var candidates = Process.GetProcesses().Where(x => !processSnapshot.Contains(x.Id));
+                            foreach (var process in candidates)
                             {
                                 try
                                 {
-                                    if (p.HasExited)
-                                    return false;
-
-                                    var pName = p.ProcessName;
-                                    if (NamesOfIgnoredProcesses.Any(n =>
-                                        pName.Equals(n, StringComparison.InvariantCultureIgnoreCase)))
-                                        return false;
+                                    if (process.MainModule.FileName.Contains(UninstallerEntry.InstallLocation,
+                                        StringComparison.InvariantCultureIgnoreCase)
+                                        ||
+                                        process.GetCommandLine()
+                                            .Contains(UninstallerEntry.InstallLocation,
+                                                StringComparison.InvariantCultureIgnoreCase))
+                                        watchedProcesses.Add(process);
                                 }
-                                catch (Win32Exception)
+                                catch
                                 {
-                                    return false;
+                                    // Ignore permission and access errors
                                 }
-                                catch (InvalidOperationException)
-                                {
-                                    return false;
-                                }
+                            }
 
-                                return !processSnapshot.Contains(p.Id);
-                            }).ToList();
-
-                            // Check if we are done, or if there are some proceses left that we missed
                             if (watchedProcesses.Count == 0)
-                            {
-                                if (string.IsNullOrEmpty(UninstallerEntry.InstallLocation))
-                                    break;
-
-                                var candidates = Process.GetProcesses().Where(x => !processSnapshot.Contains(x.Id));
-                                foreach (var process in candidates)
-                                {
-                                    try
-                                    {
-                                        if (process.MainModule.FileName.Contains(UninstallerEntry.InstallLocation,
-                                            StringComparison.InvariantCultureIgnoreCase)
-                                            ||
-                                            process.GetCommandLine()
-                                                .Contains(UninstallerEntry.InstallLocation,
-                                                    StringComparison.InvariantCultureIgnoreCase))
-                                            watchedProcesses.Add(process);
-                                    }
-                                    catch
-                                    {
-                                        // Ignore permission and access errors
-                                    }
-                                }
-
-                                if (watchedProcesses.Count == 0)
-                                    break;
-                            }
-
-                            // Check for deadlocks during silent uninstall
-                            if (checkCounters)
-                            {
-                                var processNames = watchedProcesses.Select(x =>
-                                {
-                                    try
-                                    {
-                                        return x.ProcessName;
-                                    }
-                                    catch
-                                    {
-                                        // Ignore errors caused by processes that exited
-                                        return null;
-                                    }
-                                }).Where(x => !string.IsNullOrEmpty(x));
-
-                                if (TestUninstallerForStalls(processNames))
-                                    idleCounter++;
-                                else
-                                    idleCounter = 0;
-
-                                // Kill the uninstaller (and children) if they were idle/stalled for too long
-                                if (idleCounter > 30)
-                                {
-                                    KillProcesses(watchedProcesses);
-                                    throw new IOException(Localisation.UninstallError_UninstallerTimedOut);
-                                }
-                            }
-                            else Thread.Sleep(1000);
-
-                            // Kill the uninstaller (and children) if user told us to or if it was idle for too long
-                            if (_skipLevel == SkipCurrentLevel.Terminate)
-                            {
-                                if (UninstallerEntry.UninstallerKind == UninstallerType.Msiexec)
-                                    watchedProcesses.AddRange(Process.GetProcessesByName("Msiexec"));
-
-                                KillProcesses(watchedProcesses);
                                 break;
-                            }
                         }
 
-                        if (_skipLevel == SkipCurrentLevel.None)
+                        // Check for deadlocks during silent uninstall
+                        if (checkCounters)
                         {
-                            var exitVar = uninstaller.ExitCode;
-                            if (exitVar != 0)
+                            var processNames = watchedProcesses.Select(x =>
                             {
-                                if (UninstallerEntry.UninstallerKind == UninstallerType.Msiexec && exitVar == 1602)
+                                try
                                 {
-                                    // 1602 ERROR_INSTALL_USEREXIT - The user has cancelled the installation.
-                                    _skipLevel = SkipCurrentLevel.Skip;
+                                    return x.ProcessName;
                                 }
-                                else if (UninstallerEntry.UninstallerKind == UninstallerType.Nsis &&
-                                         (exitVar == 1 || exitVar == 2))
+                                catch
                                 {
-                                    // 1 - Installation aborted by user (cancel button)
-                                    // 2 - Installation aborted by script (often after user clicks cancel)
-                                    _skipLevel = SkipCurrentLevel.Skip;
+                                    // Ignore errors caused by processes that exited
+                                    return null;
                                 }
-                                else if (exitVar == -1073741510)
-                                {
-                                    /* 3221225786 / 0xC000013A / -1073741510 
+                            }).Where(x => !string.IsNullOrEmpty(x));
+
+                            if (TestUninstallerForStalls(processNames))
+                                idleCounter++;
+                            else
+                                idleCounter = 0;
+
+                            // Kill the uninstaller (and children) if they were idle/stalled for too long
+                            if (idleCounter > 30)
+                            {
+                                KillProcesses(watchedProcesses);
+                                throw new IOException(Localisation.UninstallError_UninstallerTimedOut);
+                            }
+                        }
+                        else Thread.Sleep(1000);
+
+                        // Kill the uninstaller (and children) if user told us to or if it was idle for too long
+                        if (_skipLevel == SkipCurrentLevel.Terminate)
+                        {
+                            if (UninstallerEntry.UninstallerKind == UninstallerType.Msiexec)
+                                watchedProcesses.AddRange(Process.GetProcessesByName("Msiexec"));
+
+                            KillProcesses(watchedProcesses);
+                            break;
+                        }
+                    }
+
+                    if (_skipLevel == SkipCurrentLevel.None)
+                    {
+                        var exitVar = uninstaller.ExitCode;
+                        if (exitVar != 0)
+                        {
+                            if (UninstallerEntry.UninstallerKind == UninstallerType.Msiexec && exitVar == 1602)
+                            {
+                                // 1602 ERROR_INSTALL_USEREXIT - The user has cancelled the installation.
+                                _skipLevel = SkipCurrentLevel.Skip;
+                            }
+                            else if (UninstallerEntry.UninstallerKind == UninstallerType.Nsis &&
+                                     (exitVar == 1 || exitVar == 2))
+                            {
+                                // 1 - Installation aborted by user (cancel button)
+                                // 2 - Installation aborted by script (often after user clicks cancel)
+                                _skipLevel = SkipCurrentLevel.Skip;
+                            }
+                            else if (exitVar == -1073741510)
+                            {
+                                /* 3221225786 / 0xC000013A / -1073741510 
                                     The application terminated as a result of a CTRL+C. 
                                     Indicates that the application has been terminated either by user's 
                                     keyboard input CTRL+C or CTRL+Break or closing command prompt window. */
-                                    _skipLevel = SkipCurrentLevel.Terminate;
-                                }
-                                else
+                                _skipLevel = SkipCurrentLevel.Terminate;
+                            }
+                            else
+                            {
+                                switch (exitVar)
                                 {
-                                    switch (exitVar)
-                                    {
-                                        // The system cannot find the file specified. Indicates that the file can not be found in specified location.
-                                        case 2:
-                                        // The system cannot find the path specified. Indicates that the specified path can not be found.
-                                        case 3:
-                                        // Access is denied. Indicates that user has no access right to specified resource.
-                                        case 5:
-                                        // Program is not recognized as an internal or external command, operable program or batch file. 
-                                        case 9009:
-                                            break;
-                                        default:
-                                            if (options.RetryFailedQuiet)
-                                                retry = true;
-                                            break;
-                                    }
-                                    throw new IOException(Localisation.UninstallError_UninstallerReturnedCode +
-                                                          exitVar);
+                                    // The system cannot find the file specified. Indicates that the file can not be found in specified location.
+                                    case 2:
+                                    // The system cannot find the path specified. Indicates that the specified path can not be found.
+                                    case 3:
+                                    // Access is denied. Indicates that user has no access right to specified resource.
+                                    case 5:
+                                    // Program is not recognized as an internal or external command, operable program or batch file. 
+                                    case 9009:
+                                        break;
+                                    default:
+                                        if (options.RetryFailedQuiet)
+                                            retry = true;
+                                        break;
                                 }
+                                throw new IOException(Localisation.UninstallError_UninstallerReturnedCode +
+                                                      exitVar);
                             }
                         }
                     }
