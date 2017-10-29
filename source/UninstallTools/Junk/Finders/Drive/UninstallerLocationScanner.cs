4e7dbe89dcd379421e0ea19800628efb4a91be7c
Marcin Szeniak <marc200333@gmail.com>
2017-10-29 10:36:01 +0100

    Added scanning for MSI junk in Windows\Installer folder; Partial #85

--- source/UninstallTools/Junk/Finders/Drive/UninstallerLocationScanner.cs
+++ source/UninstallTools/Junk/Finders/Drive/UninstallerLocationScanner.cs
@@ -5,8 +5,10 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Klocman.Extensions;
+using UninstallTools.Junk.Confidence;
 using UninstallTools.Junk.Containers;
 using UninstallTools.Properties;
 
@@ -14,17 +16,47 @@ namespace UninstallTools.Junk.Finders.Drive
 {
     public class UninstallerLocationScanner : JunkCreatorBase
     {
+        private IEnumerable<string> _allProgramFiles;
+
+        public override void Setup(ICollection<ApplicationUninstallerEntry> allUninstallers)
+        {
+            _allProgramFiles = UninstallToolsGlobalConfig.GetAllProgramFiles().ToList();
+            base.Setup(allUninstallers);
+        }
+
         public override IEnumerable<IJunkResult> FindJunk(ApplicationUninstallerEntry target)
         {
             var uninLoc = target.UninstallerLocation;
-            if (!uninLoc.IsNotEmpty()) yield break;
-
-            if (UninstallToolsGlobalConfig.GetAllProgramFiles().Any(x => uninLoc.StartsWith(x, StringComparison.InvariantCultureIgnoreCase))
+            if (string.IsNullOrEmpty(uninLoc)) yield break;
+            
+            if (_allProgramFiles.Any(x => uninLoc.StartsWith(x, StringComparison.InvariantCultureIgnoreCase))
                 && !CheckIfDirIsStillUsed(uninLoc, GetOtherInstallLocations(target)))
             {
                 var resultNode = GetJunkNodeFromLocation(Enumerable.Empty<string>(), uninLoc, target);
                 if (resultNode != null)
                     yield return resultNode;
+            }
+            else if (target.UninstallerKind == UninstallerType.Msiexec && !target.BundleProviderKey.IsEmpty())
+            {
+                FileSystemInfo[] matchedItems;
+                try
+                {
+                    var winInstallerDir = new DirectoryInfo(uninLoc);
+
+                    matchedItems = winInstallerDir.GetFileSystemInfos($"*{target.BundleProviderKey}*");
+                }
+                catch (SystemException e)
+                {
+                    Console.WriteLine(e);
+                    yield break;
+                }
+
+                foreach (var fileSystemInfo in matchedItems)
+                {
+                    var junk = new FileSystemJunk(fileSystemInfo, target, this);
+                    junk.Confidence.Add(ConfidenceRecord.ExplicitConnection);
+                    yield return junk;
+                }
             }
         }
 
