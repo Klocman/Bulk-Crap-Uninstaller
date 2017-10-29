68479993192c538139866387d4d1a87495560b29
Marcin Szeniak <marc200333@gmail.com>
2017-10-29 13:20:49 +0100

    Added MSI junk detection for SOFTWARE\Classes\Installer (Products, Features, Patches); Partial #85

--- source/UninstallTools/Junk/Finders/Registry/UninstallerKeySearcher.cs
+++ source/UninstallTools/Junk/Finders/Registry/UninstallerKeySearcher.cs
@@ -3,7 +3,12 @@
     Apache License Version 2.0
 */
 
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Klocman.Extensions;
+using Klocman.IO;
 using UninstallTools.Junk.Confidence;
 using UninstallTools.Junk.Containers;
 using UninstallTools.Properties;
@@ -12,17 +17,53 @@ namespace UninstallTools.Junk.Finders.Registry
 {
     public class UninstallerKeySearcher : IJunkCreator
     {
+        private static readonly string[] InstallerSubkeyPaths = {
+                @"SOFTWARE\Classes\Installer\Products",
+                @"SOFTWARE\Classes\Installer\Features",
+                @"SOFTWARE\Classes\Installer\Patches"
+            };
+
+        /// <summary>
+        /// parent key path, upgrade code(key name)
+        /// </summary>
+        private List<KeyValuePair<string, string>> _targetKeys;
+
         public void Setup(ICollection<ApplicationUninstallerEntry> allUninstallers)
         {
+            _targetKeys = InstallerSubkeyPaths
+                .Using(x => Microsoft.Win32.Registry.LocalMachine.OpenSubKey(x))
+                .Where(k => k != null)
+                .SelectMany(k =>
+                {
+                    var parentPath = k.Name;
+                    return k.GetSubKeyNames().Select(n => new KeyValuePair<string,string>(parentPath, n));
+                }).ToList();
         }
 
         public IEnumerable<IJunkResult> FindJunk(ApplicationUninstallerEntry target)
         {
-            if (!target.RegKeyStillExists()) yield break;
+            if (target.RegKeyStillExists())
+            {
+                var regKeyNode = new RegistryKeyJunk(target.RegistryPath, target, this);
+                regKeyNode.Confidence.Add(ConfidenceRecord.IsUninstallerRegistryKey);
+                yield return regKeyNode;
+            }
 
-            var regKeyNode = new RegistryKeyJunk(target.RegistryPath, target, this);
-            regKeyNode.Confidence.Add(ConfidenceRecord.IsUninstallerRegistryKey);
-            yield return regKeyNode;
+            if (target.UninstallerKind == UninstallerType.Msiexec && !target.BundleProviderKey.IsEmpty())
+            {
+                var upgradeKey = MsiTools.ConvertBetweenUpgradeAndProductCode(target.BundleProviderKey).ToString("N");
+
+                var matchedKeyPaths = _targetKeys
+                    .Where(x => x.Value.Equals(upgradeKey, StringComparison.OrdinalIgnoreCase));
+
+                foreach (var keyPath in matchedKeyPaths)
+                {
+                    var fullKeyPath = Path.Combine(keyPath.Key, keyPath.Value);
+                    var result = new RegistryKeyJunk(fullKeyPath, target, this);
+                    result.Confidence.Add(ConfidenceRecord.ExplicitConnection);
+                    yield return result;
+                }
+            }
         }
 
         public string CategoryName => Localisation.Junk_UninstallerKey_GroupName;
