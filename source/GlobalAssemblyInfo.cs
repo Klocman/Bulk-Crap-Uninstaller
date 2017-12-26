54f53a5fba3e4a5eaed9a9502a8248e6559f43dd
Marcin Szeniak <marc200333@gmail.com>
2017-12-26 21:45:03 +0100

    Version bump to 4.0

--- source/GlobalAssemblyInfo.cs
+++ source/GlobalAssemblyInfo.cs
@@ -28,4 +28,4 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("4.0.0.*")]
+[assembly: AssemblyVersion("3.23.0.*")]
