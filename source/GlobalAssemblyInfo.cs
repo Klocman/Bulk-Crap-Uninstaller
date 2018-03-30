6f7aae91a548e09d7246e5e4685e595c3825f83c
Marcin Szeniak <marc200333@gmail.com>
2018-03-30 21:35:52 +0200

    Version bump to 4.3.2

--- source/GlobalAssemblyInfo.cs
+++ source/GlobalAssemblyInfo.cs
@@ -28,4 +28,4 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("4.3.1.*")]
+[assembly: AssemblyVersion("4.3.2.*")]
