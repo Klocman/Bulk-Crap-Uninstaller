0b5f60c0d1cd57280c45aea376559114c61318ea
Marcin Szeniak <marc200333@gmail.com>
2018-09-17 18:28:07 +0200

    Merged fr translation update by Thierry Delaunay and Orphée V.

--- source/BulkCrapUninstaller/Forms/Windows/AboutBox.cs
+++ source/BulkCrapUninstaller/Forms/Windows/AboutBox.cs
@@ -46,7 +46,7 @@ namespace BulkCrapUninstaller.Forms
                 new {culture =  CultureInfo.GetCultureInfo("es"), translator = "MS-PC2 & Freddynic159 & Emilio J. Grao"},
 
                 // fr - French
-                new {culture =  CultureInfo.GetCultureInfo("fr"), translator = "Thierry Delaunay"},
+                new {culture =  CultureInfo.GetCultureInfo("fr"), translator = "Thierry Delaunay & Orphée V."},
 
                 // Hungarian
                 new {culture =  CultureInfo.GetCultureInfo("hu"), translator = "Phoenix (Döbröntei Sándor)"},
