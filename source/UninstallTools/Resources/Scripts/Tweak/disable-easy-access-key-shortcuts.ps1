a8001188e0cb1329201a9165b621116811ebf0b8
Marcin Szeniak <marc200333@gmail.com>
2018-09-19 20:41:35 +0200

    Added script to disable easy keys shortcuts

---
+++ source/UninstallTools/Resources/Scripts/Tweak/disable-easy-access-key-shortcuts.ps1
@@ -0,0 +1,3 @@
+Set-ItemProperty "HKCU:\Control Panel\Accessibility\StickyKeys" "Flags" "506"
+Set-ItemProperty "HKCU:\Control Panel\Accessibility\Keyboard Response" "Flags" "122"
+Set-ItemProperty "HKCU:\Control Panel\Accessibility\ToggleKeys" "Flags" "58"
