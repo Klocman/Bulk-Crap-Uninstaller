2c2bdde83c623be71959526242acfb2b5e0b24a3
Marcin Szeniak <marc200333@gmail.com>
2021-05-11 21:24:32 +0200

    Set up publish process for x84 and x64

--- publish.bat
+++ publish.bat
@@ -1,17 +1,47 @@
+
+rem Release Debug
+set config=Release
+
 set msbuild="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MSBuild.exe"
 set publish=%CD%\bin\publish
-set target=%CD%\bin\publish
 
-del /f /s /q "%target%"
 
-%msbuild% "source\BulkCrapUninstaller.sln" /m /p:filealignment=512 /t:Restore;Rebuild /p:DeployOnBuild=true /p:PublishSingleFile=False /p:SelfContained=True /p:PublishProtocol=FileSystem /p:Configuration=Release /p=Platform:"Any CPU" /p:TargetFrameworks=net5.0-windows10.0.18362.0 /p:PublishDir="%target%" /p:RuntimeIdentifier=win-x64 /p:PublishReadyToRun=false /p:PublishTrimmed=False /verbosity:minimal
+rmdir /q /s "%publish%"
+if errorlevel 1 (pause)
+rmdir /q /s bin\launcher
+if errorlevel 1 (pause)
 
-%msbuild% "source\BulkCrapUninstaller.sln" /m /p:filealignment=512 /t:Publish /p:DeployOnBuild=true /p:PublishSingleFile=False /p:SelfContained=True /p:PublishProtocol=FileSystem /p:Configuration=Release /p:Platform="Any CPU" /p:TargetFrameworks=net5.0-windows10.0.18362.0 /p:PublishDir="%target%" /p:RuntimeIdentifier=win-x64 /p:PublishReadyToRun=false /p:PublishTrimmed=False /verbosity:minimal
 
-rem mv "%target%\BCU_manual.html" "%publish%\BCU_manual.html"
-rem mv "%target%\Licence.txt" "%publish%\Licence.txt"
-rem mv "%target%\PrivacyPolicy.txt" "%publish%\PrivacyPolicy.txt"
+set platform=x64
+call :publish
 
-del /f /s /q "%target%\*.pdb"
+set platform=x86
+call :publish
+
+copy bin\launcher\BCU-launcher.exe %publish%\BCUninstaller.exe
+copy "%target%\BCU_manual.html" "%publish%\BCU_manual.html"
+copy "%target%\Licence.txt" "%publish%\Licence.txt"
+copy "%target%\PrivacyPolicy.txt" "%publish%\PrivacyPolicy.txt"
+
+rmdir /q /s bin\launcher
+
+IF %config%==Release (del /f /s /q "%publish%\*.pdb")
 
 pause
+exit
+
+
+
+rem -------------------------------------------------------------
+
+:publish
+set identifier=win-%platform%
+set target=%CD%\bin\publish\%identifier%
+
+%msbuild% "source\BulkCrapUninstaller.sln" /m /p:filealignment=512 /t:Restore;Rebuild /p:DeployOnBuild=true /p:PublishSingleFile=False /p:SelfContained=True /p:PublishProtocol=FileSystem /p:Configuration=%config% /p:Platform=%platform% /p:TargetFrameworks=net5.0-windows10.0.18362.0 /p:PublishDir="%target%" /p:RuntimeIdentifier=%identifier% /p:PublishReadyToRun=false /p:PublishTrimmed=False /verbosity:minimal
+
+%msbuild% "source\BulkCrapUninstaller.sln" /m /p:filealignment=512 /t:Publish /p:DeployOnBuild=true /p:PublishSingleFile=False /p:SelfContained=True /p:PublishProtocol=FileSystem /p:Configuration=%config% /p:Platform=%platform% /p:TargetFrameworks=net5.0-windows10.0.18362.0 /p:PublishDir="%target%" /p:RuntimeIdentifier=%identifier% /p:PublishReadyToRun=false /p:PublishTrimmed=False /verbosity:minimal
+
+goto :eof
+
+rem -------------------------------------------------------------
