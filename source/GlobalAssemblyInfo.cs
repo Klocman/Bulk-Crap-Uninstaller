80651dbdc4c598b91844a35a37bd4b06b4a137a9
Marcin Szeniak <14913904+Klocman@users.noreply.github.com>
2022-02-07 17:17:09 +0100

    Version up to 5.2

--- source/GlobalAssemblyInfo.cs
+++ source/GlobalAssemblyInfo.cs
@@ -37,4 +37,4 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("5.1.0.0")]
+[assembly: AssemblyVersion("5.2.0.0")]
