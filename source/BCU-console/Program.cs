a622401f19db55ae39b3a7a551e30e78c27bbc9e
Zafer Balkan <zafer@zaferbalkan.com>
2022-04-27 15:58:50 +0300

    Implemented list command

--- source/BCU-console/Program.cs
+++ source/BCU-console/Program.cs
@@ -33,6 +33,8 @@ BCU-console export [drive:][path]filename [/Q] [/U] [/V] - Export installed appl
  [drive:][path]	– Specifies drive and directory to where the export should be saved.
  filename       – Specifies filename of the .xml file to save the exported application information to.
 
+BCU-console list /Q] [/U] [/V] - Display a list of installed applications.
+
 Switches:
  /Q             - Use quiet uninstallers wherever possible (by default only use loud).
  /U             - Unattended mode (do not ask user for confirmation). WARNING: ONLY USE AFTER
@@ -73,12 +75,12 @@ Return codes:
                 {
                     case "uninstall":
                         return ProcessUninstallCommand(args.Skip(1).ToArray());
-
+                    case "list":
+                        return ProcessListCommand(args.Skip(1).ToArray());
                     case "export":
                         return ProcessExportCommand(args.Skip(1).ToArray());
-
                     default:
-                        Console.WriteLine("Invalid command \"{0}\"\n", args[0]);
+                        Console.WriteLine($"Invalid command \"{args[0]}\"\n");
                         ShowHelp();
                         return 1;
                 }
@@ -91,13 +93,59 @@ Return codes:
             }
         }
 
+        private static int ProcessListCommand(string[] args)
+        {
+            var isVerbose = args.Any(x => x.Equals("/V", StringComparison.OrdinalIgnoreCase));
+            var isQuiet = args.Any(x => x.Equals("/Q", StringComparison.OrdinalIgnoreCase));
+            var isUnattended = args.Any(x => x.Equals("/U", StringComparison.OrdinalIgnoreCase));
+
+            args = args.Where(x => !x.StartsWith("/", StringComparison.Ordinal)).ToArray();
+            if (args.Length != 1)
+                return ShowInvalidSyntaxError("Missing export filename or invalid arguments");
+
+            var apps = QueryApps(isQuiet, isUnattended, isVerbose);
+            var serializer = new ApplicationEntrySerializer(apps);
+
+            Console.WriteLine(string.Format("{0,-40}  {1,-20}  {2,-40}", "Display Name", "Version", "Source"));
+            var sb = new StringBuilder(82);
+            for (int i = 0; i < 82; i++)
+            {
+                sb.Append('-');
+            }
+            Console.WriteLine(sb.ToString());
+
+            foreach (var entry in serializer.Items)
+            {
+                var displayName = entry.DisplayNameTrimmed;
+                if (displayName.Length > 40)
+                {
+                    displayName = displayName.Substring(0, 40);
+                }
+
+                var version = entry.DisplayVersion ?? string.Empty;
+                if (version.Length > 20)
+                {
+                    version = version.Substring(0, 20);
+                }
+
+                var source = entry.AboutUrl ?? string.Empty;
+                if (source.Length > 40)
+                {
+                    source = source.Substring(0, 40);
+                }
+
+                Console.WriteLine(string.Format("{0,-40}  {1,-20}  {2,-40}", displayName, version, source));
+            }
+            return 0;
+        }
+
         private static int ProcessExportCommand(string[] args)
         {
             var isVerbose = args.Any(x => x.Equals("/V", StringComparison.OrdinalIgnoreCase));
             var isQuiet = args.Any(x => x.Equals("/Q", StringComparison.OrdinalIgnoreCase));
             var isUnattended = args.Any(x => x.Equals("/U", StringComparison.OrdinalIgnoreCase));
 
-            args = args.Where(x => !x.StartsWith("/")).ToArray();
+            args = args.Where(x => !x.StartsWith("/", StringComparison.Ordinal)).ToArray();
             if (args.Length != 1)
                 return ShowInvalidSyntaxError("Missing export filename or invalid arguments");
 
@@ -192,8 +240,7 @@ Return codes:
 
                     foreach (var error in task.AllUninstallersList.Where(x =>
                         x.CurrentStatus != UninstallStatus.Completed && x.CurrentError != null))
-                        Console.WriteLine("Error: {0} - {1}", error.UninstallerEntry.DisplayName,
-                            error.CurrentError.Message);
+                        Console.WriteLine($"Error: {error.UninstallerEntry.DisplayName} - {error.CurrentError.Message}");
                 }
             };
             task.Start();
