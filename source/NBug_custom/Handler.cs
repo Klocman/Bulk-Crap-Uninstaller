693765fb211745ad0587f7383f75954019f05b08
Marcin Szeniak <14913904+Klocman@users.noreply.github.com>
2026-09-07 23:57:54 +0200

    Show a better error message for missing or blocked BCU files and do not offer to send

--- source/NBug_custom/Handler.cs
+++ source/NBug_custom/Handler.cs
@@ -5,7 +5,8 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 using System;
-using System.Diagnostics.Tracing;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows.Forms;
 using Microsoft.Win32.Interop;
@@ -61,7 +62,10 @@ namespace NBug
         /// <param name="e">Real exception is in: e.Exception</param>
         private static void ThreadExceptionHandler(object sender, ThreadExceptionEventArgs e)
         {
-            if (HandleOutdatedWindowsCrash(e.Exception) && Settings.HandleExceptions)
+            if (HandleOutdatedWindowsCrash(e.Exception)) return;
+            if (HandleMissingApplicationFiles(e.Exception)) return;
+
+            if (Settings.HandleExceptions)
             {
                 Logger.Trace("Starting to handle a System.Windows.Forms.Application.ThreadException.");
 
@@ -82,17 +86,22 @@ namespace NBug
         /// <param name="e">Real exception is in: ((Exception)e.ExceptionObject)</param>
         private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
         {
-            if (HandleOutdatedWindowsCrash(e.ExceptionObject as Exception) && Settings.HandleExceptions)
+            var exception = (Exception)e.ExceptionObject;
+
+            if (HandleOutdatedWindowsCrash(exception)) return;
+            if (HandleMissingApplicationFiles(exception)) return;
+
+            if (Settings.HandleExceptions)
             {
                 Logger.Trace("Starting to handle a System.AppDomain.UnhandledException.");
-                var executionFlow = new BugReport().Report((Exception) e.ExceptionObject, ExceptionThread.Main);
+                var executionFlow = new BugReport().Report(exception, ExceptionThread.Main);
                 if (executionFlow == ExecutionFlow.BreakExecution)
                 {
                     Environment.Exit(0);
                 }
             }
         }
-        
+
         private static bool HandleOutdatedWindowsCrash(Exception dnfe)
         {
             // DllNotFoundException, EntryPointNotFoundException, possibly others
@@ -105,11 +114,72 @@ namespace NBug
                                     "Unsupported Windows Version", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     // Do not let NBug handle this, it only clogs up the error reports
                     Environment.Exit((int)ResultWin32.ERROR_DLL_NOT_FOUND);
-                    return false;
+                    return true;
                 }
             }
 
-            return true;
+            return false;
+        }
+
+        private static bool HandleMissingApplicationFiles(Exception ex)
+        {
+            if (ex != null)
+            {
+                // Unwrap first
+                if (ex is AggregateException agex)
+                {
+                    foreach (var inner in agex.Flatten().InnerExceptions)
+                    {
+                        if (inner is FileNotFoundException or FileLoadException)
+                        {
+                            ex = inner;
+                            break;
+                        }
+                    }
+                }
+                else if (ex is InvalidOperationException iopex && iopex.InnerException is not null)
+                    ex = iopex.InnerException;
+
+                // If the exception is because of a missing file it's most likely because of an AV or other security software
+                if (ex is FileNotFoundException fnfex)
+                    return Handle(fnfex.FileName);
+                // Usually caused by AVs blocking access
+                if (ex is FileLoadException flex)
+                    return Handle(flex.FileName);
+            }
+
+            return false;
+
+
+            bool Handle(string filename)
+            {
+                // Handle missing assemblies which are referred by qualified name
+                if (Regex.IsMatch(filename, @"^\w[\w\.]*, Version="))
+                {
+                    ShowMessage(filename.Split(',')[0]);
+                    return true;
+                }
+                // Handle missing files which are referred by path
+                // Ensure the file is inside the app directory to not catch unrelated exceptions
+                if (string.Equals(Path.GetDirectoryName(filename), NBug.Settings.NBugDirectory, StringComparison.OrdinalIgnoreCase))
+                {
+                    ShowMessage(filename);
+                    return true;
+                }
+
+                return false;
+            }
+
+            void ShowMessage(string filename)
+            {
+                MessageBox.Show("It seems like some of the BCUninstaller files are missing or inaccessible. " +
+                                "Please make sure you have all of the BCUninstaller files in the same directory and " +
+                                "that your antivirus or other software is not preventing BCU from running.\n\n" +
+                                "Could not access file: " + filename,
+                    "Missing or inaccessible BCUninstaller files", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Do not let NBug handle this, it only clogs up the error reports
+                Environment.Exit((int)ResultWin32.ERROR_DLL_NOT_FOUND);
+            }
         }
     }
 }
